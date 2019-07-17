d5f9778ed0b6185408bdf04b3b48da4f3cb6d115
Joakim Repomaa <mail@j.repomaa.com>
2019-07-17 12:04:55 +0200

    add block invocations

--- spec/memoized_spec.cr
+++ spec/memoized_spec.cr
@@ -10,6 +10,35 @@ describe Memoized do
     msg.get.should eq(cnt)
   end
 
+  it "can be invoked with a block" do
+    msg = Memoized(Int32).new do
+      Dir["/tmp/*"].size
+    end
+
+    cnt = msg.get
+    cnt.should be_a(Int32)
+  end
+
+  it "can be invoked with a timespan and a block" do
+    msg = Memoized(Int32).new(1.minute) do
+      Dir["/tmp/*"].size
+    end
+
+    cnt = msg.get
+    cnt.should be_a(Int32)
+    msg.get.should eq(cnt)
+  end
+
+  it "can be invoked with a path and a block" do
+    msg = Memoized(Int32).new(__FILE__) do
+      Dir["/tmp/*"].size
+    end
+
+    cnt = msg.get
+    cnt.should be_a(Int32)
+    msg.get.should eq(cnt)
+  end
+
   it "#clear" do
     always = Memoized(Int32).new(int_adder)
     always.get.should eq(1)
--- src/memoized/new.cr
+++ src/memoized/new.cr
@@ -2,12 +2,24 @@ class Memoized(T)
   def self.new(loader : -> T)
     new(loader, Always.new)
   end
-  
+
+  def self.new(&loader : -> T)
+    new(loader)
+  end
+
   def self.new(loader : -> T, span : Time::Span)
     new(loader, Finite.new(span))
   end
 
+  def self.new(span : Time::Span, &loader : -> T)
+    new(loader, span)
+  end
+
   def self.new(loader : -> T, path : String)
     new(loader, Source.new(path))
+  end
+
+  def self.new(path : String, &loader : -> T)
+    new(loader, path)
   end
 end
